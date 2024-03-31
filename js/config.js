3b3cf43b0bc2137a6b96b343350cc0d214069ada
Ferah <ferah.redjeb1@outlook.com>
2024-03-31 18:54:24 +1100

    add invoke url for api gateway to call lambda function

--- js/config.js
+++ js/config.js
@@ -1,10 +1,10 @@
 window._config = {
     cognito: {
         userPoolId: 'ap-southeast-2_NnILAl4sp',
-        userPoolClientId: '2bat3flt3lt2jbhr6h3jovobdr', //
-        region: 'ap-southeast-2' //
+        userPoolClientId: '2bat3flt3lt2jbhr6h3jovobdr',
+        region: 'ap-southeast-2'
     },
     api: {
-        invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
+        invokeUrl: 'https://9bw7gykj2j.execute-api.ap-southeast-2.amazonaws.com/prod'
     }
 };
